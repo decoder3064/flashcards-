e5bc259cff0367a284eef62961c1b942c19ceb76
David R. <dreyes@colgate.edu>
2025-09-29 12:18:43 -0400

    added css rules to make my project look good

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,23 @@ import Cardportal from './assets/components/cardportal'
 function App() {
 
   const cards = [
-  {id:1,front:"placeholder1", back:"hello there"},
-  {id:2,front:"placeholder2", back:"hello there"},
-  {id:3,front:"placeholder3", back:"hello there"},
-  {id:4,front:"placeholder4", back:"hello there"},
-  {id:5,front:"placeholder5", back:"hello there"},
-  {id:6,front:"placeholder6", back:"hello there"},
-  {id:7,front:"placeholder7", back:"hello there"},
-  {id:8,front:"placeholder8", back:"hello there"},
-  {id:9,front:"placeholder9", back:"hello there"},
-  {id:10,front:"placeholder10", back:"hello there"},
+  {id:1,front:"Why don't scientists trust atoms?", back:"Because they make up everything!"},
+  {id:2,front:"What do you call a fake noodle?", back:"An impasta!"},
+  {id:3,front:"Why did the scarecrow win an award?", back:"He was outstanding in his field!"},
+  {id:4,front:"What do you call a bear with no teeth?", back:"A gummy bear!"},
+  {id:5,front:"Why don't eggs tell jokes?", back:"They'd crack each other up"},
+  {id:6,front:"What do you call a sleeping bull?", back:"A bulldozer!"},
+  {id:7,front:"Why did the bicycle fall over?", back:"It was two tired!"},
+  {id:8,front:"What do you call cheese that isn't yours?", back:"Nacho cheese!"},
+  {id:9,front:"Why can't you hear a pterodactyl using the bathroom?", back:"Because the 'P' is silent!"},
+  {id:10,front:"This is the last card lil bro.", back:"There is no joke here"},
 ]
 
   return (
     <>
-      <div> This is a Flash Card Game</div>
+      <h1 className='title1'> Wanna have fun? </h1>
+      <h3 className='title2'>Look at this cards and laugh please they are funny I swear 😩</h3>
+      <h4 className='title3'> Number of Jokes:{cards.length} </h4>
       <Cardportal cards = {cards}/>
     </>
   )
--- src/assets/components/Cardportal.jsx
+++ src/assets/components/Cardportal.jsx
@@ -6,11 +6,21 @@ function Cardportal(props){
     const changeIndex = () =>{
         setNextCard(nextCard+1)
     }
+    
+     if (nextCard >= props.cards.length) {
+        return (
+            <div className="end-screen">
+                <h2 className="title3">🎉 Congrats lil bro! YOu HaVe ReaChed ThE EnD 🎉</h2>
+                <img className="end-gif"src="images/end.gif" />
+                <button onClick={() => setNextCard(0)}>Start Over</button>
+            </div>
+        )
+    }
 
 
     return (
     <>
-        <Flashcard content={props.cards[nextCard]} ></Flashcard>
+        <Flashcard content={props.cards[nextCard]}></Flashcard>
         <button onClick={changeIndex}>Next</button>
     </>
     )
